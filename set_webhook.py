d7e7e9b96401b5d70f9153f2731914516ce79ec5
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-28 23:03:35 +0530

    Delete set_webhook.py

--- set_webhook.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
-response = requests.post(url, json={"url": f"{WEBHOOK_URL}/webhook"})
-print(response.json())
