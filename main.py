f6d219508a31d57b8c6b7b1811606f100b71a876
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-28 23:02:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import os
 from texts import get_text, TEXTS
 from buttons import format_buttons
 from utils import detect_platform, download_media
-await bot.delete_webhook(drop_pending_updates=True)
 
 asyncio.run(clear_webhook())
 # Bot Token and Mongo
