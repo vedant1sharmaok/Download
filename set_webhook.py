6729c03579476f9901b1b08fcb7903d562316cb1
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-30 18:27:33 +0530

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -7,8 +7,8 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 if not BOT_TOKEN or not WEBHOOK_URL:
     raise ValueError("BOT_TOKEN and WEBHOOK_URL must be set as environment variables")
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
-payload = {"url": f"{WEBHOOK_URL}/webhook"}
+url = f"https://api.telegram.org/bot6670175533:AAHx8MCXwliT7NZE370I2E15Z-Sp5_0V1s4/setWebhook"
+payload = {"url": f"https://puzzled-felicia-heroxgfxmovie-4a7dac59.koyeb.app/webhook"}
 
 response = requests.post(url, json=payload)
 print("Status:", response.status_code)
